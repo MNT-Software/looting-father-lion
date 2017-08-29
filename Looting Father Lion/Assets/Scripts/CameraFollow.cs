b3ef18ff5f668ce2ee518649cc941c76ea869960
MaelstromALPHA <maelstrom.alpha@gmail.com>
2017-08-29 14:38:40 +0100

    Camera dampening
    Added a camera dampening script to smooth out camera motion. Also added a staircase trigger to my movement test scene, although not implemented in script yet.

---
+++ Looting Father Lion/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	// Declared variables for use in script
+	// Private
+	private Vector3 velocity = Vector3.zero;
+
+	// Public
+	public float dampTime = 0.15f;
+	public Transform target;
+
+	void FixedUpdate () 
+	{
+		// Camera dampening based on the position of a target object (camera anchor point)
+		// This serves to smooth the camera movement by delaying the camera movement slightly based on velocity
+		if (target)
+		{
+			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
+	}
+}
