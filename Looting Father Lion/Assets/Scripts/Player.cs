619cffe23c9833cea9c99f65482d57a9e7e9d5bb
MaelstromALPHA <maelstrom.alpha@gmail.com>
2017-09-03 17:05:43 +0100

    Final changes for the day

--- Looting Father Lion/Assets/Scripts/Player.cs
+++ Looting Father Lion/Assets/Scripts/Player.cs
@@ -27,7 +27,7 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Renderer> ().material.color = new Color (0, 255, 0);
+		// GetComponent<Renderer> ().material.color = new Color (0, 255, 0);
 		gameManager = GameObject.Find ("Game Manager");
 		proximitySound = gameObject.GetComponentInChildren<SphereCollider> ();
 		stairs = false;
